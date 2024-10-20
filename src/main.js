d0783690b8ccc4d444a45f3aad838d40fc4262c4
tim <83369306+tim-martinez@users.noreply.github.com>
2024-10-19 21:12:33 -0400

    initial commit and vite config.

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// src/sum.js
+export function sum(a, b) {
+  return a + b;
+}
+
+console.log('battleship');
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    globals: true, // Enables global test variables like `describe`, `it`, and `expect`
+    environment: 'jsdom', // Allows DOM API testing
+    setupFiles: './src/setupTests.js', // Optional: path for setup file
+  },
+  server: {
+    open: true, // This will open the browser automatically
+  },
+});
